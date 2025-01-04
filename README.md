0ce32c6ea3fb8b4f7114525aab1c89159bea9b43
uint0 <i@uint0.dev>
2025-01-04 11:52:00 +1100

    chore: respect totems

--- README.md
+++ README.md
@@ -2,7 +2,5 @@
 
 A minecraft mod that makes death more impactful but doesn't completely delete your world. Current changes:
 
-1. Upon death, a player's inventory is cleared and no items are dropped
-
-**Warning: This mod currently cannot be toggled on or off per world and applies to all worlds**
+1. Upon death, a player's inventory is cleared and no items are dropped. Gamerule: `nqhClearInventoryOnDeath` (default: `false`)
 
--- src/main/kotlin/dev/uint0/notquitehardcore/NotQuiteHardcore.kt
+++ src/main/kotlin/dev/uint0/notquitehardcore/NotQuiteHardcore.kt
@@ -3,24 +3,49 @@ package dev.uint0.notquitehardcore
 import org.slf4j.LoggerFactory
 import net.fabricmc.api.ModInitializer
 import net.fabricmc.fabric.api.entity.event.v1.ServerLivingEntityEvents
+import net.fabricmc.fabric.api.gamerule.v1.GameRuleRegistry
+import net.fabricmc.fabric.api.gamerule.v1.GameRuleFactory
+import net.fabricmc.loader.impl.FabricLoaderImpl
 import net.minecraft.entity.Entity
 import net.minecraft.entity.damage.DamageSource
 import net.minecraft.entity.player.PlayerEntity
+import net.minecraft.entity.LivingEntity
 import net.minecraft.server.network.ServerPlayerEntity
+import net.minecraft.server.world.ServerWorld
+import net.minecraft.util.ActionResult
+import net.minecraft.item.Items
+import net.minecraft.world.GameRules
 
 object NotQuiteHardcore : ModInitializer {
     private val logger = LoggerFactory.getLogger("notquitehardcore")
 
-	override fun onInitialize() {
-		logger.info("Initializing Not Quite Hardcore")
+    val GR_CLEAR_INVENTORY_ON_DEATH = GameRuleRegistry.register(
+        "nqhClearInventoryOnDeath",
+        GameRules.Category.PLAYER,
+        GameRuleFactory.createBooleanRule(false)
+    );
 
-		ServerLivingEntityEvents.ALLOW_DEATH.register(this::onPlayerDeath)
+
+	override fun onInitialize() {
+        logger.info("Initializing Not Quite Hardcore")
+
+        ServerLivingEntityEvents.ALLOW_DEATH.register(this::onPlayerDeath)
 	}
 
-	private fun onPlayerDeath(entity: Entity, source: DamageSource, damageAmount: Float): Boolean {
+	private fun onPlayerDeath(entity: LivingEntity, source: DamageSource, damageAmount: Float): Boolean {
         if (entity is ServerPlayerEntity) {
+            val world = entity.getWorld() as? ServerWorld ?: return true
+
+            if(!world.getGameRules().getBoolean(GR_CLEAR_INVENTORY_ON_DEATH)) {
+                return true;
+            }
+            if (entity.getInventory().contains(Items.TOTEM_OF_UNDYING.getDefaultStack())) {
+                return true;
+            }
+
             entity.getInventory().clear();
         }
+
         return true;
     }
 }
